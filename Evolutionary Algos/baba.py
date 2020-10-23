34ad2434cb3471c9b3abd5436fd983529ee83195
qazi112 <qarsalan13@gmail.com>
2020-10-23 16:22:52 +0000

    First Commit

---
+++ Evolutionary Algos/baba.py
@@ -0,0 +1,21 @@
+# from PIL import Image
+# from matplotlib import image
+# from matplotlib import pyplot
+
+# import numpy as np
+# from numpy import asarray
+# # load image as pixel array
+
+# image = image.imread('abc.png')
+
+# im = Image.open('abc.png')
+# a = np.asarray(im)
+
+
+# im = Image.fromarray(a)
+# print(type(im))
+# print(a)
+# im.show()
+# pyplot.imshow(image)
+# pyplot.show()
+
